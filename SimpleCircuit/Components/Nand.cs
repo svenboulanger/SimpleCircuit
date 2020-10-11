d6f6c5db9cefeeca1d8eb2648b631f5bca6cd036
Sven Boulanger <svenboulanger@hotmail.com>
2020-10-11 13:40:59 +0200

    Add NAND, NOR and SPDT

---
+++ SimpleCircuit/Components/Nand.cs
@@ -0,0 +1,47 @@
+﻿namespace SimpleCircuit.Components
+{
+    /// <summary>
+    /// And gate.
+    /// </summary>
+    /// <seealso cref="TransformingComponent" />
+    [SimpleKey("NAND", "Nand gate.", Category = "Digital")]
+    public class Nand : TransformingComponent
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Or"/> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        public Nand(string name)
+            : base(name)
+        {
+            Pins.Add(new[] { "a", "in1" }, "First input.", new Vector2(-6, 2), new Vector2(-1, 0));
+            Pins.Add(new[] { "b", "in2" }, "Second input.", new Vector2(-6, -2), new Vector2(-1, 0));
+            Pins.Add(new[] { "o", "out" }, "Output.", new Vector2(9, 0), new Vector2(1, 0));
+        }
+
+        /// <inheritdoc />
+        public override void Render(SvgDrawing drawing)
+        {
+            var normal = new Vector2(NormalX.Value, NormalY.Value);
+            var tf = new Transform(X.Value, Y.Value, normal, normal.Perpendicular * Scale.Value);
+
+            drawing.ClosedBezier(tf.Apply(new[]
+            {
+                new Vector2(-6, 5),
+                new Vector2(-6, 5), new Vector2(1, 5), new Vector2(1, 5),
+                new Vector2(4, 5), new Vector2(6, 2), new Vector2(6, 0),
+                new Vector2(6, -2), new Vector2(4, -5), new Vector2(1, -5),
+                new Vector2(1, -5), new Vector2(-6, -5), new Vector2(-6, -5)
+            }));
+            drawing.Circle(tf.Apply(new Vector2(7.5, 0)), 1.5);
+        }
+
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString() => $"And {Name}";
+    }
+}
---
+++ SimpleCircuit/Components/SwitchSinglePoleDoubleThrow.cs
@@ -0,0 +1,47 @@
+﻿namespace SimpleCircuit.Components
+{
+    /// <summary>
+    /// Single-pole double throw switch.
+    /// </summary>
+    /// <seealso cref="TransformingComponent" />
+    /// <seealso cref="ILabeled" />
+    [SimpleKey("SPDT", "Single-pole double throw switch", Category = "Analog")]
+    public class SwitchSinglePoleDoubleThrow : TransformingComponent, ILabeled
+    {
+        /// <inheritdoc/>
+        public string Label { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SwitchSinglePoleDoubleThrow"/> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        public SwitchSinglePoleDoubleThrow(string name)
+            : base(name)
+        {
+            Pins.Add(new[] { "p" }, "The pole pin.", new Vector2(-8, 0), new Vector2(-1, 0));
+            Pins.Add(new[] { "c", "ctrl" }, "The controlling pin.", new Vector2(0, 6), new Vector2(0, 1));
+            Pins.Add(new[] { "t1" }, "The first throwing pin.", new Vector2(8, 4), new Vector2(1, 0));
+            Pins.Add(new[] { "t2" }, "The second throwing pin.", new Vector2(8, -4), new Vector2(1, 0));
+        }
+
+        public override void Render(SvgDrawing drawing)
+        {
+            var normal = new Vector2(NormalX.Value, NormalY.Value);
+            var tf = new Transform(X.Value, Y.Value, normal, normal.Perpendicular * Scale.Value);
+
+            drawing.Segments(tf.Apply(new[]
+            {
+                new Vector2(-8, 0), new Vector2(-6, 0),
+                new Vector2(6, 4), new Vector2(8, 4),
+                new Vector2(6, -4), new Vector2(8, -4)
+            }));
+            drawing.Circle(tf.Apply(new Vector2(-5, 0)), 1);
+            drawing.Circle(tf.Apply(new Vector2(5, 4)), 1);
+            drawing.Circle(tf.Apply(new Vector2(5, -4)), 1);
+            drawing.Line(tf.Apply(new Vector2(-4, 0)), tf.Apply(new Vector2(4, 4)));
+
+            if (Pins.IsUsed("c"))
+                drawing.Line(tf.Apply(new Vector2(0, 2)), tf.Apply(new Vector2(0, 6)));
+        }
+    }
+}
