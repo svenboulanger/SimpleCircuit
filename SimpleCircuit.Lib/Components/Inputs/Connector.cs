c651810c3041d9a39ee016a0825ee2cbbfd9559a
Sven Boulanger <svenboulanger@hotmail.com>
2022-07-03 17:57:37 +0200

    Add ANSI style connectors

--- SimpleCircuit.Lib/Components/Inputs/Connector.cs
+++ SimpleCircuit.Lib/Components/Inputs/Connector.cs
@@ -9,6 +9,9 @@ namespace SimpleCircuit.Components.Inputs
     [Drawable("CONN", "A connector or fastener.", "Inputs")]
     public class Connector : DrawableFactory
     {
+        private const string _male = "male";
+        private const string _female = "female";
+
         /// <inheritdoc />
         protected override IDrawable Factory(string key, string name)
             => new Instance(name);
@@ -30,17 +33,92 @@ namespace SimpleCircuit.Components.Inputs
             {
                 Pins.Add(new FixedOrientedPin("negative", "The negative pin.", this, new(-4, 0), new(-1, 0)), "n", "neg", "b");
                 Pins.Add(new FixedOrientedPin("positive", "The positive pin.", this, new(2, 0), new(1, 0)), "p", "pos", "a");
+                Variants.Changed += UpdatePins;
             }
 
             /// <inheritdoc />
             protected override void Draw(SvgDrawing drawing)
             {
-                drawing.ExtendPin(Pins["n"]);
-                drawing.ExtendPin(Pins["p"], 4);
-                drawing.Circle(new(), 1.5);
-                drawing.Arc(new(), Math.PI / 4, -Math.PI / 4, 4, null, 3);
+                switch (Variants.Select(Options.Ansi))
+                {
+                    case 0:
+                        switch (Variants.Select(_male, _female))
+                        {
+                            case 0:
+                                drawing.ExtendPin(Pins["n"], 5);
+                                drawing.Polyline(new Vector2[]
+                                {
+                                    new(4, 4), new(), new(4, -4)
+                                });
+                                if (Pins["p"].Connections > 0)
+                                    drawing.Text(Label, new(5, 1), new(1, 1));
+                                else
+                                    drawing.Text(Label, new(5, 0), new(1, 0));
+                                break;
 
-                drawing.Text(Label, new(0, -5), new(0, -1));
+                            case 1:
+                                drawing.ExtendPin(Pins["n"], 5);
+                                drawing.Polyline(new Vector2[]
+                                {
+                                    new(-4, 4), new(), new(-4, -4)
+                                });
+                                if (Pins["p"].Connections > 0)
+                                    drawing.Text(Label, new(1, 1), new(1, 1));
+                                else
+                                    drawing.Text(Label, new(1, 0), new(1, 0));
+                                break;
+
+                            default:
+                                drawing.ExtendPins(Pins, 5);
+                                drawing.Polyline(new Vector2[]
+                                {
+                                    new(-6, 4), new(-2, 0), new(-6, -4)
+                                });
+                                drawing.Polyline(new Vector2[]
+                                {
+                                    new(-2, 4), new(2, 0), new(-2, -4)
+                                });
+                                drawing.Text(Label, new(0, -5), new(0, -1));
+                                break;
+                        }
+                        break;
+
+                    default:
+                        drawing.ExtendPin(Pins["n"]);
+                        drawing.ExtendPin(Pins["p"], 4);
+                        drawing.Circle(new(), 1.5);
+                        drawing.Arc(new(), Math.PI / 4, -Math.PI / 4, 4, null, 3);
+
+                        drawing.Text(Label, new(0, -5), new(0, -1));
+                        break;
+                }
+            }
+
+            private void UpdatePins(object sender, EventArgs e)
+            {
+                switch (Variants.Select(Options.Ansi))
+                {
+                    case 0:
+                        switch (Variants.Select(_male, _female))
+                        {
+                            case 0:
+                            case 1:
+                                SetPinOffset(0, new());
+                                SetPinOffset(1, new());
+                                break;
+
+                            default:
+                                SetPinOffset(0, new(-2, 0));
+                                SetPinOffset(1, new(2, 0));
+                                break;
+                        }
+                        break;
+
+                    default:
+                        SetPinOffset(0, new());
+                        SetPinOffset(1, new());
+                        break;
+                }
             }
         }
     }
