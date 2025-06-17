929df127c3cca6126ee6bae638b788a482e323f0
Sven Boulanger <svenboulanger@hotmail.com>
2025-06-17 21:08:31 +0200

    Write more demo's

--- SimpleCircuitOnline/Demo.cs
+++ SimpleCircuitOnline/Demo.cs
@@ -44,7 +44,7 @@
                             new Demo(
                                 "A2. Properties and variants",
                                 "Tutorial explaning the basics of component properties and variants.",
-                                "* --- Properties and variants ---\r\n\r\n* Variants are like tags that you can attach to a component\r\nR\r\nR(programmable)\r\nR(thermistor)\r\nR(euro, memristor)\r\n\r\n* Properties are key-value pairs you can pass to a component\r\nR(zigs=5)\r\n\r\n* For a list of variants and properties, go to\r\n*   Help > Components\r\n* to have a list of available components with properties/variants."),
+                                "* --- Properties and variants ---\r\n\r\n* Variants are like tags that you can attach to a component\r\nR\r\nR(programmable)\r\nR(thermistor)\r\nR(euro, memristor)\r\n\r\n* Properties are key-value pairs you can pass to a component\r\nR(zigs=5)\r\n\r\n* If you want to have all components matching a filter to \r\n* have certain properties of variants, you can use the\r\n* '.properties' or '.variants' control statement\r\n* These control statements only act in the current scope,\r\n* so if you try moving the '.properties' statement out\r\n* of the scope block, all of the components will turn green\r\n.scope\r\n    .properties * color=\"green\"\r\n    .variants L programmable\r\n    R L C\r\n.ends\r\n\r\n* For a list of variants and properties, go to\r\n*   Help > Components\r\n* to have a list of available components with properties/variants.\r\n"),
                             new Demo(
                                 "A3. Wires",
                                 "Tutorial explaining the basics of wires.",
@@ -70,19 +70,23 @@
                     new("Advanced") {
                         Items = [
                             new Demo(
-                                "B1. Sections",
+                                "B1. Parameters and Expression",
+                                "Tutorial on parameters, expressions, if-else statements and for-loops.",
+                                "* --- Variables and Expressions ---\r\n\r\n* Like in SPICE, '.param' statements can define parameters\r\n* These can in turn be used in expressions that are placed\r\n* between '{' and '}'\r\n* Also like SPICE, .param statements don't have to appear\r\n* before being used. They are simply defined for the\r\n* current scope\r\nR(label1={myLabel})\r\n.param myLabel = \"Hello World\"\r\n\r\n* Parameters can also be used in component names\r\n.param componentName = \"DCmyName\"\r\nA{componentName} <r> T(out)\r\nADCmyName(\"Label\")\r\n\r\n* Parameters can also be used in if-else statements\r\n.param case = 1\r\n.if {case == 0}\r\n    C(\"Case = 0\")\r\n.else\r\n    C(\"Case != 0\")\r\n.endif\r\n\r\n* Parameters can also be defined in a scope by a for-loop\r\n.for i 0 5 1\r\n    X{i} <r +5> R(label1={\"R_\" + i}) <r +5> X{i + 1}\r\n.endf"),
+                            new Demo(
+                                "B2. Sections",
                                 "Tutorial explaining how sections work",
                                 "* --- Sections ---\r\n\r\n* A section is a group of wires and components that are\r\n* grouped together and are less easily accessed from the\r\n* outside. Sections can have parameters.\r\n.section SEC fg=\"--primary\"\r\n    .property * fg={fg}\r\n    .property wire fg={fg}\r\n\r\n    * Any named components are relative to the section\r\n    * Other sections can reuse the name 'Vin' for example\r\n    GND <u> Vin(\"Input 1\") <u r> R <r> S <r d> C <d> GND\r\n\r\n    * The virtual chain will only treat anonymous components\r\n    * inside the same section\r\n    (y GND)\r\n.ends\r\n\r\n* A previous section can be copied using the format:\r\n.section SEC2 SEC(fg=\"--secondary\")\r\n\r\n* It is possible to access elements inside sections using '/'\r\nSEC2/Vin(\"Input 2\")\r\n\r\n* Alignment across sections is also possible this way\r\n(y SEC/Vin <r> SEC2/Vin)\r\n"),
                             new Demo(
-                                "B2 - Subcircuits",
+                                "B3 - Subcircuits",
                                 "Tutorial explaining how subcircuits work",
                                 "* --- Subcircuits ---\r\n\r\n* Subcircuits are solved as a separate mini-circuit and\r\n* will then act as a component on their own.\r\n* They should have ports, and can be followed by parameters.\r\n* For example:\r\n.subckt LPF DIRin[a] DIRout[b] r=\"1k\" c=\"1uF\" fg=\"--foreground\"\r\n    DIRin <r 5> R(label1={r}, fg={fg}) <r fg={fg} x r 5> DIRout\r\n    X <d fg={fg}> C(label1={c}, fg={fg}) <d fg={fg}> GND(fg={fg})\r\n.ends\r\n\r\n* The subcircuit name (LPF) can be used as a component key\r\n* The parameters can be passed like properties\r\nT(in, \"V_in\") <r> LPF1 <r> X1\r\nX1 <ne> LPF(r=\"2k\", fg=\"--primary\") <ne> T(\"V_out1\", out)\r\nX1 <se> LPF(c=\"2u\", fg=\"--secondary\") <se> T(\"V_out2\", out)"),
                             new Demo(
-                                "B3 - Queued Anonymous Points",
+                                "B4 - Queued Anonymous Points",
                                 "Tutorial explaining how to use queued anonymous points",
                                 "* --- Queued Anonymous Points ---\r\n\r\n* In many cases we would like to mark down a quick point\r\n* that we can use later\r\n* Inside a wire definition, the letter 'x' will queue\r\n* an anonymous point, that can be picked up again when\r\n* an anonymous point is created with 'X'\r\n\r\nT(in) <r x r> C <r x r> T(out)\r\n\r\n* We have queued up two points with the previous chain\r\n* We will now dequeue these two points in the following\r\n* statement. The order of the X'es are important!\r\nX <u r> S <r d> X"),
                             new Demo(
-                                "B4 - Black Boxes",
+                                "B5 - Black Boxes",
                                 "Tutorial explaning how to use black boxes with dynamic pin names.",
                                 "* --- Black Boxes ---\r\n\r\n* Black Boxes ('BB') are components that we don't know the pins\r\n* of beforehand. Pins of a 'BB' are created dynamically.\r\n\r\n* The following chain creates 2 pins with the name 'a' and 'b'\r\n* The order is important! The first pin will appear top or left of\r\n* the component. The wire that connects to the pin determines\r\n* on which side the pin should be added.\r\nBB1[a] <r d> R <d l> [b]BB1\r\n\r\n* The first underscore character '_' has special meaning\r\n* Everything before the first '_' will be used as an alias for the pin\r\n* Everything after the first '_' will be displayed as the pin name.\r\n* The whole pin name can always be used as a reference, and\r\n* without '_', the whole name is used as the pin\r\nBB1[_shown] <d r> L <r u> [hidden_]BB1\r\n\r\n* Using an expression, you can pretty make anything act as\r\n* the pin text.\r\nBB1[special_{\"\\overline{v^2}\"}] <l> T(in)\r\n"),
                         ]
